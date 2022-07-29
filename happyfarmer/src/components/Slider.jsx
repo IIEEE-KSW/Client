d605b2d21e65b329608fa672770709fe01877d3f
gayoungyeom <gayoungyeom@gmail.com>
2022-07-29 10:50:29 -0400

    [RFT] (hotfix) - refact range mimmax setting

--- happyfarmer/src/components/Slider.jsx
+++ happyfarmer/src/components/Slider.jsx
@@ -23,82 +23,47 @@ const handle = (props) => {
   );
 };
 
+const rangeMinMax = {
+  Temperature: {
+    min: 0,
+    max: 105,
+  },
+  Humidity: {
+    min: 0,
+    max: 100,
+  },
+  Pressure: {
+    min: 10,
+    max: 50,
+  },
+  Windspeed: {
+    min: 0,
+    max: 40,
+  },
+};
+
 const Sliders = ({ range, setRange, rangeVal, title }) => {
   const onSliderChange = (val) => {
     const value = { lower: val[0], upper: val[1] };
     setRange(value);
   };
 
-  let marks, min, max;
-  if (title == 'Temperature') {
-    marks = {
-      0: {
-        label: '0',
-        style: {
-          fontFamily: 'poppinsR',
-        },
+  const min = rangeMinMax[title].min;
+  const max = rangeMinMax[title].max;
+  const marks = {
+    [min]: {
+      label: min,
+      style: {
+        fontFamily: 'poppinsR',
       },
-      104: {
-        label: '104',
-        style: {
-          fontFamily: 'poppinsR',
-        },
+    },
+    [max]: {
+      label: max,
+      style: {
+        fontFamily: 'poppinsR',
       },
-    };
-    min = 0;
-    max = 104;
-  } else if (title == 'Humidity') {
-    marks = {
-      0: {
-        label: '0',
-        style: {
-          fontFamily: 'poppinsR',
-        },
-      },
-      100: {
-        label: '100',
-        style: {
-          fontFamily: 'poppinsR',
-        },
-      },
-    };
-    min = 0;
-    max = 100;
-  } else if (title == 'Pressure') {
-    marks = {
-      10: {
-        label: '10',
-        style: {
-          fontFamily: 'poppinsR',
-        },
-      },
-      50: {
-        label: '50',
-        style: {
-          fontFamily: 'poppinsR',
-        },
-      },
-    };
-    min = 10;
-    max = 50;
-  } else if (title == 'Windspeed') {
-    marks = {
-      0: {
-        label: '0',
-        style: {
-          fontFamily: 'poppinsR',
-        },
-      },
-      37: {
-        label: '37',
-        style: {
-          fontFamily: 'poppinsR',
-        },
-      },
-    };
-    min = 0;
-    max = 37;
-  }
+    },
+  };
 
   return (
     <Range
