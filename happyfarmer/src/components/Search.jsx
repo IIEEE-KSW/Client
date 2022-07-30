8ca9e73579b093cd7129d564bafde39a83e4f709
dazzel3 <aakk9350@naver.com>
2022-07-30 15:19:26 -0400

    [FIX] (hotfix) - fix css in search

--- happyfarmer/src/components/Search.jsx
+++ happyfarmer/src/components/Search.jsx
@@ -69,8 +69,7 @@ const Container = styled.div`
   width: 50vh;
   position: relative;
   @media screen and (max-width: 767px) and (orientation: portrait) {
-    width: 90%;
-    margin: 5% 0 0 0;
+    width: 100%;
   }
 `;
 
